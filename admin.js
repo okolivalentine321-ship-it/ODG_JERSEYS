d3604cee5929f4a22cb25026a5d23ee4317ce613
okolivalentine321-ship-it <okolivalentine321@gmail.com>
2026-09-14 12:49:51 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,10 @@
+const SUPABASE_URL = "https://fbsvgzvwzkceyoekhvna.supabase.co";
+const SUPABASE_KEY = "YOUR_SUPABASE_KEY";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Admin Supabase connected");
 
